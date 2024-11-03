197ade761f0027311005c9fbc776de410fdbe003
Aymen Abdellaoui <aymen.abdellaoui997@gmail.com>
2024-11-03 04:21:48 +0100

    fix: transpose word with previous/next word in the previous/next line

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,3 +7,15 @@ Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how 
 ## [Unreleased]
 
 - Initial release
+
+## [0.0.4] - 2024-11-3
+
+### Fixed
+
+- Fixed bug where the transposed word was at the end/beginning of the line, now it is correctly transposed with the word before/after it in the previous/next line. (#2)
+
+## [0.0.3] - 2024-01-30
+
+### Fixed
+
+- Fixed bug where the cursor was not correctly positioned after transposing the word. (#1)
--- src/handlers/transpose-words.ts
+++ src/handlers/transpose-words.ts
@@ -1,35 +1,5 @@
 import * as vscode from "vscode";
-const swapWord = (lineText: string, cursorPosition: number) => {
-  const textBeforeCursor = lineText.slice(0, cursorPosition);
-  const textAfterCursor = lineText.slice(cursorPosition);
-  const emptySpaceBeforeCursor = textBeforeCursor.match(/\s+$/);
-  const emptySpaceAfterCursor = textAfterCursor.match(/^\s+/);
-  const emptySpaceBeforeCursorLength = emptySpaceBeforeCursor
-    ? emptySpaceBeforeCursor[0].length
-    : 0;
-  const emptySpaceAfterCursorLength = emptySpaceAfterCursor
-    ? emptySpaceAfterCursor[0].length
-    : 0;
-  const previousWord = getPreviousWord(textBeforeCursor).trimEnd();
-  const nextWord = getNextWord(textAfterCursor).trimStart();
-  const newTextBeforeCursor = replaseLastOccurrence(
-    textBeforeCursor.trimEnd(),
-    previousWord,
-    nextWord
-  );
-  const newTextAfterCursor = textAfterCursor
-    .trimStart()
-    .replace(nextWord, previousWord);
-  const newLineText =
-    newTextBeforeCursor +
-    " ".repeat(emptySpaceBeforeCursorLength + emptySpaceAfterCursorLength) +
-    newTextAfterCursor;
-  const newCursorPosition = Math.max(
-    0,
-    newTextBeforeCursor.length + emptySpaceBeforeCursorLength
-  );
-  return { newLineText, newCursorPosition };
-};
+
 const getNextWord = (text: string) => {
   const words = text.match(/\b\w+\b/g);
   return words && words.length > 0 ? words[0] : "";
@@ -39,9 +9,10 @@ const getPreviousWord = (text: string) => {
   return words && words.length > 0 ? words[words.length - 1] : "";
 };
 
+
 const isString = (input: unknown): input is string => typeof input === "string";
 
-const replaseLastOccurrence = (
+const replaceLastOccurrence = (
   input: string,
   find: string,
   replaceWith: string
@@ -66,10 +37,107 @@ const replaseLastOccurrence = (
 const transpose_words_handler = (editor: vscode.TextEditor) => {
   const position = editor.selection.active;
   const currentLine = editor.document.lineAt(position.line).text;
-  const { newLineText, newCursorPosition } = swapWord(
-    currentLine,
-    position.character
+  const cursorPosition = position.character;
+  const lineText = editor.document.lineAt(position.line).text;
+
+  const textBeforeCursor = lineText.slice(0, cursorPosition);
+  const textAfterCursor = lineText.slice(cursorPosition);
+  if (textBeforeCursor.trim() === "" ) {
+    if (position.line === 0) {
+      return;
+    }
+     const lastLine = editor.document.lineAt(position.line - 1).text || "";
+     if (lastLine.trim() === "" ) {
+       return;
+     }
+     const lastLineLastWord = getPreviousWord(lastLine);
+     const currentLineFirstWord = getNextWord(lineText);
+      const newLastLine = replaceLastOccurrence(lastLine, lastLineLastWord, currentLineFirstWord);
+      const newCurrentLine = lineText.replace(currentLineFirstWord, lastLineLastWord);
+      const newCursorPosition = Math.max(
+        0,
+        newCurrentLine.length
+      );
+     editor.edit((edit) => {
+        edit.replace(
+          new vscode.Range(position.line - 1, 0, position.line, currentLine.length),
+          newLastLine + "\n" + newCurrentLine
+        );
+      }).then(() => {
+        if (vscode.window.activeTextEditor)
+          vscode.window.activeTextEditor.selection = new vscode.Selection(
+            position.line,
+            newCursorPosition,
+            position.line,
+            newCursorPosition
+          );
+      });
+      return;
+  }
+
+  if (textAfterCursor.trim() === "") {
+    const count = editor.document.lineCount;
+    if (position.line + 1 === count) {
+      return;
+    }
+    const nextLine = editor.document.lineAt(position.line + 1).text || "";
+    if (nextLine.trim() === "") {
+      return;
+    }
+    const nextLineFirstWord = getNextWord(nextLine);
+    const currentLineLastWord = getPreviousWord(lineText);
+    const newNextLine = nextLine.replace(nextLineFirstWord, currentLineLastWord);
+    const newCurrentLine = replaceLastOccurrence(lineText, currentLineLastWord, nextLineFirstWord);
+    const newCursorPosition = Math.max(
+      0,
+      newCurrentLine.length
+    );
+     editor.edit((edit) => {
+      edit.replace(
+        new vscode.Range(position.line, 0, position.line + 1, nextLine.length),
+        newCurrentLine + "\n" + newNextLine
+      );
+    }).then(() => {
+      if (vscode.window.activeTextEditor)
+        vscode.window.activeTextEditor.selection = new vscode.Selection(
+          position.line,
+          newCursorPosition,
+          position.line,
+          newCursorPosition
+        );
+    });
+    return;
+  }
+  const emptySpaceBeforeCursor = textBeforeCursor.match(/\s+$/);
+  const emptySpaceAfterCursor = textAfterCursor.match(/^\s+/);
+  const emptySpaceBeforeCursorLength = emptySpaceBeforeCursor
+    ? emptySpaceBeforeCursor[0].length
+    : 0;
+  const emptySpaceAfterCursorLength = emptySpaceAfterCursor
+    ? emptySpaceAfterCursor[0].length
+    : 0;
+
+
+  const previousWord = getPreviousWord(textBeforeCursor).trimEnd();
+  const nextWord = getNextWord(textAfterCursor).trimStart();
+
+  const newTextBeforeCursor = replaceLastOccurrence(
+    textBeforeCursor.trimEnd(),
+    previousWord,
+    nextWord
   );
+  const newTextAfterCursor = textAfterCursor
+    .trimStart()
+    .replace(nextWord, previousWord);
+  const newLineText =
+    newTextBeforeCursor +
+    " ".repeat(emptySpaceBeforeCursorLength + emptySpaceAfterCursorLength) +
+    newTextAfterCursor;
+  const newCursorPosition = Math.max(
+    0,
+    newTextBeforeCursor.length + emptySpaceBeforeCursorLength
+  );
+
   editor
     .edit((edit) => {
       edit.replace(
